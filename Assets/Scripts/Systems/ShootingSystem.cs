bce09ac4f15642803856ce22af0da43bf7dcc49b
SamEmam <samhoffmann111@hotmail.com>
2019-02-21 14:33:31 +0100

    ShootingSystem troubleshooting
    InvalidOperationsException: The NativeArray has been deallocated.

--- Assets/Scripts/Systems/ShootingSystem.cs
+++ Assets/Scripts/Systems/ShootingSystem.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
 
 public class ShootingSystem : ComponentSystem
 {
@@ -19,7 +21,24 @@ public class ShootingSystem : ComponentSystem
         {
             if (entity.weaponC.fireCountdown <= 0f)
             {
-                Object.Instantiate(entity.weaponC.shotPrefab, entity.weaponC.firePoint.position, entity.weaponC.firePoint.rotation);
+                //var manager = World.Active.GetOrCreateManager<EntityManager>();
+
+                //var shotArchetype = manager.CreateArchetype(typeof(Position), typeof(Rotation));
+
+                //Entity shotEntity = manager.CreateEntity(shotArchetype);
+
+                //manager.SetComponentData(shotEntity, new Position { Value = entity.weaponC.firePoint.position });
+                //manager.SetComponentData(shotEntity, new Rotation { Value = entity.weaponC.firePoint.rotation });
+                //NativeArray<Entity> entities = new NativeArray<Entity>(1, Allocator.Temp);
+                //manager.Instantiate(entity.weaponC.shotPrefab,entities);
+                //for (int i = 0; i < 1; i++)
+                //{
+                //    manager.SetComponentData(entities[i], new Position { Value = entity.weaponC.firePoint.position });
+                //    manager.SetComponentData(entities[i], new Rotation { Value = entity.weaponC.firePoint.rotation });
+                //}
+                //entities.Dispose();
+
+                var shot = Object.Instantiate(entity.weaponC.shotPrefab, entity.weaponC.firePoint.position, entity.weaponC.firePoint.rotation);
 
                 entity.weaponC.fireCountdown = 1f / entity.weaponC.fireRate;
             }
