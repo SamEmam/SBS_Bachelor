1a42111afe70c7773072a9de72ee6e2d609de377
mdamsgaard1 <mattias.damsgaard@me.com>
2019-03-18 15:19:38 +0100

    Update Asteroid

--- Assets/Scripts/Systems/SpawnSystem.cs
+++ Assets/Scripts/Systems/SpawnSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
@@ -12,10 +13,11 @@ public class SpawnSystem : ComponentSystem
         public ComponentArray<AsteroidComponent> asteroidC;
         public ComponentArray<Transform> transfrom;
         public EntityArray entities;
+        [NativeDisableParallelForRestriction]public EntityCommandBuffer commandBuffer;
     }
 
     [Inject] private Components components;
-    
+
 
     protected override void OnUpdate()
     {
@@ -26,16 +28,31 @@ public class SpawnSystem : ComponentSystem
             var spawnC = components.spawnC[i];
             var asteroidC = components.asteroidC[i];
             var transform = components.transfrom[i];
+            var commandB = components.commandBuffer;
 
             // Functionality
+
+
             for (int j = 0; j < spawnC.numberOfSpawns; j++)
             {
-                var asteroid = Object.Instantiate(asteroidC.asteroidPrefab, transform.position, transform.rotation);                            // Instantiate asteroid
-                asteroid.transform.position = Random.insideUnitSphere * (spawnC.minDistance + (spawnC.maxDistance - spawnC.minDistance));       // Set position within sphere
-                var size = Random.Range(asteroidC.minSize, asteroidC.maxSize);                                                                  // Randomize size
+                var pos = spawnC.transform.position = Random.insideUnitSphere * (spawnC.minDistance + (spawnC.maxDistance - spawnC.minDistance));       // Set position within sphere
+                var size = Random.Range(asteroidC.minSize, asteroidC.maxSize);
+                while (!Physics.CheckSphere(pos, size / 2f))
+                {
+                    pos = spawnC.transform.position = Random.insideUnitSphere * (spawnC.minDistance + (spawnC.maxDistance - spawnC.minDistance));       // Set position within sphere
+                    size = Random.Range(asteroidC.minSize, asteroidC.maxSize);
+                    Debug.Log(j + "collide on spawn");
+
+                }
+                // Instantiate asteroid
+                var asteroid = Object.Instantiate(asteroidC.asteroidPrefab, transform.position, transform.rotation);
                 asteroid.transform.localScale = new Vector3(size, size, size);                                                                  // Set size
-                asteroid.transform.rotation = Random.rotation;                                                                                  // Randomize rotation
-                spawnC.numberOfSpawns--;                                                                                                        // Count down spawned asteroid
+                asteroid.transform.rotation = Random.rotation;
+                spawnC.numberOfSpawns--;
+                if (spawnC.numberOfSpawns <= 0)
+                {
+                    commandB.RemoveComponent<SpawnComponent>(entity);
+                }                                                                                  // Randomize rotation
             }
         }
     }
