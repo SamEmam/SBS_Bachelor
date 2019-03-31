3e0e71d6e6623fc3a94baf108594fd90813bd8ec
SamEmam <samhoffmann111@hotmail.com>
2019-03-31 12:11:54 +0200

    TargetInRange and Aim
    Ships now have a viewing angle for weapons, if enemy is within range, it will orient the weapon towards the enemy and shoot.
    
    Skybox also imported

---
+++ Assets/Scripts/Components/TargetInRangeComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetInRangeComponent : MonoBehaviour
+{
+    public bool isInRange;
+    public int minRange;
+    public int maxRange;
+    
+}
--- Assets/Scripts/Systems/ShootingSystem.cs
+++ Assets/Scripts/Systems/ShootingSystem.cs
@@ -11,28 +11,48 @@ public class ShootingSystem : ComponentSystem
     {
         public readonly int Length;
         public ComponentArray<WeaponComponent> weaponC;
+        public ComponentArray<AimComponent> aimC;
+        public ComponentArray<TargetInRangeComponent> targetInRangeC;
+        public ComponentArray<Transform> transform;
     }
 
     [Inject] private Components components;
-
+    
     protected override void OnUpdate()
     {
-        var deltatime = Time.deltaTime;
+        var deltaTime = Time.deltaTime;
 
         for (int i = 0; i < components.Length; i++)
         {
             // Setup
             var weaponC = components.weaponC[i];
+            var aimC = components.aimC[i];
+            var targetInRangeC = components.targetInRangeC[i];
+            var transform = components.transform[i];
 
             // Functionality
-            if (weaponC.fireCountdown <= 0f)
+            if (targetInRangeC.isInRange)
             {
-                var shot = Object.Instantiate(weaponC.shotPrefab, weaponC.firePoint.position, weaponC.firePoint.rotation);
+                var pos = aimC.target.position - transform.position;
+                Quaternion targetRotation = Quaternion.LookRotation(pos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * deltaTime);
 
-                weaponC.fireCountdown = 1f / weaponC.fireRate;
+                if (weaponC.fireCountdown <= 0f)
+                {
+                    var shot = Object.Instantiate(weaponC.shotPrefab, weaponC.firePoint.position, weaponC.firePoint.rotation);
+
+                    weaponC.fireCountdown = 1f / weaponC.fireRate;
+                }
+                
             }
 
-            weaponC.fireCountdown -= deltatime;
+            
+
+            weaponC.fireCountdown -= deltaTime;
+
+
+
+            
         }
     }
 }
