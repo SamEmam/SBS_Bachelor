496349d7b2e6182ab9cc400752085ac4b07aef61
SamEmam <samhoffmann111@hotmail.com>
2019-04-11 04:09:12 +0200

    Menu links setup
    Main menu now links correctly
    HangarScene initialized
    Settings window preparations
    Exit functionality implemented
    
    Simple UI asset package imported

---
+++ Assets/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+    public void SaveGame()
+    {
+        Debug.Log("Saving Game!");
+        //PlayerPrefs.SetInt("Currency", 0);
+    }
+
+    public void ResetGame()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
